058f431e9b76a9ea86ef90f561837b3f1b976098
Mostafa Meerzad <mostafameerzad@gmail.com>
2025-05-30 14:58:51 +0430

    install and configure prisma

---
+++ prisma/migrations/20250530092607_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Prompt" (
+    "id" SERIAL NOT NULL,
+    "prompt" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Prompt_pkey" PRIMARY KEY ("id")
+);
