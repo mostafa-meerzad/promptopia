7c53afeb5ca809919b4a533c3731611265328dc2
Mostafa Meerzad <mostafameerzad@gmail.com>
2025-06-17 23:08:29 +0430

    implement edit prompt route

---
+++ app/api/prompts/[id]/route.ts
@@ -0,0 +1,25 @@
+import { promptSchema } from "@/app/validationSchemas";
+import { prismaClient } from "@/prisma/lib/prisma";
+import { parseTags } from "@/utils/parseTags";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function PUT(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const body = await req.json();
+  const validation = promptSchema.safeParse(body);
+  const { id } = await params;
+  
+  if (!validation.success)
+    return NextResponse.json(validation.error.errors[0], { status: 400 });
+
+  const { title, content, isPublic, tags } = validation.data;
+
+  const newPrompt = await prismaClient.prompt.update({
+    where: { id: parseInt(id) },
+    data: { title, content, isPublic, tags: parseTags(tags) },
+  });
+
+  return NextResponse.json({ ok: true, prompt: newPrompt });
+}
