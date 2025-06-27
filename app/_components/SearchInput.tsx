1e1607ba3d7cf2d2a5e2aa8d9fdb62f4b83133c4
Mostafa Meerzad <mostafameerzad@gmail.com>
2025-06-27 10:57:04 +0430

    update search input

--- app/_components/SearchInput.tsx
+++ app/_components/SearchInput.tsx
@@ -3,19 +3,16 @@ import { Input, InputGroup } from "@chakra-ui/react";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { FormEvent, useState } from "react";
 import { CiSearch } from "react-icons/ci";
-import { useDebounce } from "use-debounce";
 
 const SearchInput = () => {
   const router = useRouter();
   const pathname = usePathname();
   const params = useSearchParams();
   const [txt, setTxt] = useState(params.get("q") ?? "");
-//   const [debounced] = useDebounce(txt, 400);
 
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
-    console.log("handle submit clicked", pathname);
-    router.replace(`${pathname}?q=${txt}`);
+    router.replace(`${pathname}?q=${txt.trim()}`);
   };
 
   return (
